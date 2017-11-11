084ce4af6567e77455db4e3086376d201d3ad03b
Alliston Carlos Rosa Aleixo <alliston@outlook.com>
2017-11-11 19:27:38 -0200

    Initial Commit

---
+++ BitcoinExchangeRate/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BitcoinExchangeRate
+//
+//  Created by Alliston Aleixo on 11/11/2017.
+//  Copyright © 2017 Alliston Aleixo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
